bff3c56cacde301aa326297f0a91e8010f7432c9
Anikeet-dev <aniketkakde55@.com>
2023-10-11 00:41:57 +0530

    clean up code

--- app.js
+++ app.js
@@ -1,58 +1,9 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-
-        const messages = fs.readFileSync('message.txt', 'utf8').split('\n');
-
-        res.write('<html>');
-        res.write('<head><title><Enter Message></title></head>');
-        res.write('<body>');
-
-        messages.forEach(message => {
-            res.write(`<p>${message}</p>`);
-        });
-
-        
-        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button>');
-        res.write('</body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if(url === '/message' &&  method === 'POST'){
-
-         const body = [];
-         req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-         });
-         req.on('end', () =>{
-            const parsedBody =  Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            
-            fs.writeFileSync('message.txt',message);
-            
-         });
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
-        
-    }
+const server = http.createServer(routes);
 
 
+server.listen(4000);
 
-
-    res.setHeader('Content-Type', 'text/plain');
-
- 
-});
-
-const port = 4000;
-server.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}/`);
-});
 
