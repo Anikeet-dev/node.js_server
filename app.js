1a4be790a107212ab3b618283c3b2bc12273373a
Anikeet-dev <aniketkakde55@.com>
2023-10-10 18:30:11 +0530

    reading and writing from the file

--- app.js
+++ app.js
@@ -1,22 +1,54 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 
     const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+
+        const messages = fs.readFileSync('message.txt', 'utf8').split('\n');
+
+        res.write('<html>');
+        res.write('<head><title><Enter Message></title></head>');
+        res.write('<body>');
+
+        messages.forEach(message => {
+            res.write(`<p>${message}</p>`);
+        });
+
+        
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' &&  method === 'POST'){
+
+         const body = [];
+         req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+         });
+         req.on('end', () =>{
+            const parsedBody =  Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            
+            fs.writeFileSync('message.txt',message);
+            
+         });
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        
+    }
+
+
+
 
     res.setHeader('Content-Type', 'text/plain');
 
-    if (url === '/home') {
-        res.end('Welcome home');
-    } else if (url === '/about') {
-        res.end('Welcome to About Us page');
-    } else if (url === '/node') {
-        res.end('Welcome to my Node Js project');
-    } else {
-    
-        res.statusCode = 404;
-        res.end('404 Not Found');
-    }
+ 
 });
 
 const port = 4000;
