70ca2c0ef6cf2892c9156b26e72415bdf83cd098
Anikeet-dev <aniketkakde55@.com>
2023-10-10 16:14:40 +0530

    returning custom responses.

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const http = require('http');
 
-
 const server = http.createServer((req, res) => {
-   
-    console.log('Aniket');
+
+    const url = req.url;
 
     res.setHeader('Content-Type', 'text/plain');
-   
+
+    if (url === '/home') {
+        res.end('Welcome home');
+    } else if (url === '/about') {
+        res.end('Welcome to About Us page');
+    } else if (url === '/node') {
+        res.end('Welcome to my Node Js project');
+    } else {
     
+        res.statusCode = 404;
+        res.end('404 Not Found');
+    }
 });
 
+const port = 4000;
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}/`);
+});
 
-server.listen(4000);
