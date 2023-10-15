c70d2953bb428a860a98e074ffa64b93f9b16cd6
Aniket Kakade <aniketkakde55@gmail.com>
2023-10-15 16:13:21 +0530

    Used express and middlewares

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const http = require('http');
-const routes = require('./routes');
 
-const server = http.createServer(routes);
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("In the middleware!");
+    next(); // Allows the request to continue to the next middleware in line.
+});
+
+app.use((req, res, next) => {
+    console.log("In the another middleware!");
+    res.send('<h1>Hello there,from Express!</h1>')
+});
+
 
 const port = 4000;
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}/`);
 });
 
